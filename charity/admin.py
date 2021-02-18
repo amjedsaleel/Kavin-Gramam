e8b172f7c47af95fd860fc12aba6437bbc51233c
Amjed Saleel K <amjedsaleel@gmail.com>
2021-02-18 20:05:31 +0530

    admin panel customize

--- charity/admin.py
+++ charity/admin.py
@@ -6,9 +6,30 @@ from . models import Category, Member, HouseMember, Requirement
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Member)
-admin.site.register(HouseMember)
-admin.site.register(Requirement)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category',)
+    list_display_links = ('category',)
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'house_name', 'locality',)
+    list_display_links = ('name',)
+
+
+class HouseMemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'member', 'relationship',)
+    list_display_links = ('name',)
+
+
+class RequirementAdmin(admin.ModelAdmin):
+    list_display = ('member',)
+    list_display_links = ('member',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(HouseMember, HouseMemberAdmin)
+admin.site.register(Requirement, RequirementAdmin)
 
 
